d8c570770a81bd3a754edb749fa9de8070cb0d60
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2022-08-06 10:57:50 +0100

    commit -a

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let billEl = document.querySelector("#bill");
 let noOfPeopleEl = document.querySelector("#people");
 let tipPercentages = document.querySelectorAll(".cl-tip");
+let tipAmountPerPersonEl = document.querySelector("#tip-per-person");
+let totalAmountPerPersonEl = document.querySelector("#total-per-person");
 
 let billAmount = 0;
 let noOfPeople = 0;
+let tipPercentage = 0;
 
 billEl.addEventListener("keyup", (e) => {
  billAmount = Number(e.target.value);
@@ -15,4 +18,42 @@ noOfPeopleEl.addEventListener("keyup", (e) => {
  calculateTip();
 });
 
-console.log(tipPercentages);
+Array.from(tipPercentages).forEach((tipPercentageEl) => {
+ tipPercentageEl.addEventListener("click", (e) => {
+  if (e.target.innerText.includes("%")) {
+   tipPercentage = Number(e.target.innerText.replace("%", ""));
+   applyActiveClass(e.target.innerText);
+   calculateTip();
+  }
+ });
+});
+
+function calculateTip() {
+
+ let tipAmount = billAmount * (tipPercentage / 100);
+ let totalAmount = billAmount + tipAmount;
+ let tipAmountPerPerson = tipAmount / noOfPeople;
+ let totalAmountPerPerson = totalAmount / noOfPeople;
+
+ updateValues({
+  tipAmountPerPerson,
+  totalAmountPerPerson,
+ });
+}
+
+function updateValues({ tipAmountPerPerson, totalAmountPerPerson }) {
+ tipAmountPerPersonEl.innerText =
+  tipAmountPerPerson == Infinity ? 0 : tipAmountPerPerson.toFixed(2);
+ totalAmountPerPersonEl.innerText =
+  totalAmountPerPerson == Infinity ? 0 : totalAmountPerPerson.toFixed(2);
+}
+
+function applyActiveClass(innerTextPect) {
+ Array.from(tipPercentages).forEach((tipPercentageEl) => {
+  if (tipPercentageEl.innerText == innerTextPect) {
+   tipPercentageEl.classList.add("active");
+  } else {
+   tipPercentageEl.classList.remove("active");
+  }
+ });
+}
